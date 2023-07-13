94a27614b368059fb382a3db3eaec7842592e8e0
jSohnData <jihos@me.com>
2023-07-13 13:20:34 -0400

    ADDED interface

---
+++ src/interface.ts
@@ -0,0 +1,9 @@
+interface TthreeSum {
+  num1: number;
+  num2: number;
+  message?: string;
+}
+
+export const threeSum = (obj: TthreeSum) => {
+  return console.log(`Sum = ${obj.num1 + obj.num2} ${obj.message}`);
+}
---
+++ src/intro.ts
@@ -0,0 +1,8 @@
+
+/*
+? = make it optional
+*/
+
+export const twoSum = (num1: number, num2: number, message?: string) => {
+  return console.log(`Sum = ${num1 + num2} ${message}`);
+}
